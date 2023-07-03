7d981521fe53b548f6c837d75da90a0d1fb2b2b3
Marcin Markowski <marcin.markowski@itlibrium.com>
2023-07-03 09:30:46 +0200

    better encapsulation for reference resolution scope in P3ModelSerializer

--- Sources/Parser/OutputFormatting/Json/P3ModelSerializer.cs
+++ Sources/Parser/OutputFormatting/Json/P3ModelSerializer.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using P3Model.Parser.ModelSyntax;
 using P3Model.Parser.OutputFormatting.Json.Converters;
 
@@ -8,30 +12,77 @@ namespace P3Model.Parser.OutputFormatting.Json;
 
 public static class P3ModelSerializer
 {
-    public static string Serialize(Model model)
+    private static readonly JsonConverter[] Converters =
     {
-        var options = CreateOptions();
-        return JsonSerializer.Serialize(model, options);
-    }
+        new PolymorphicJsonConverter<Element>(),
+        new PolymorphicJsonConverter<Relation>(),
+        new PolymorphicJsonConverter<Trait>(),
+        new FileInfoJsonConverter(),
+        new HierarchyIdJsonConverter()
+    };
 
-    public static async Task Serialize(Stream stream, Model model)
-    {
-        var options = CreateOptions();
-        await JsonSerializer.SerializeAsync(stream, model, options);
-    }
+    [PublicAPI]
+    public static string Serialize(Model model) => JsonSerializer.Serialize(model, CreateOptions());
 
+    [PublicAPI]
+    public static async Task Serialize(Stream stream, Model model) =>
+        await JsonSerializer.SerializeAsync(stream, model, CreateOptions());
+
+    // PolymorphicJsonConverter calls JsonSerializer.SerializeToDocument method.
+    // PreserveReferenceHandler clears reference resolution scope in each call to that method.
+    // Thus we need new JsonSerializerOptions object with ReferenceResolver that is not recreated for each JsonSerializer.Serialize method call.
     private static JsonSerializerOptions CreateOptions()
     {
         var options = new JsonSerializerOptions
         {
-            ReferenceHandler = new StaticReferenceHandler(),
+            ReferenceHandler = new SerializationScopeReferenceHandler(),
             WriteIndented = true
         };
-        options.Converters.Add(new PolymorphicJsonConverter<Element>());
-        options.Converters.Add(new PolymorphicJsonConverter<Relation>());
-        options.Converters.Add(new PolymorphicJsonConverter<Trait>());
-        options.Converters.Add(new FileInfoJsonConverter());
-        options.Converters.Add(new HierarchyIdJsonConverter());
+        foreach (var converter in Converters)
+            options.Converters.Add(converter);
         return options;
+    }
+
+    private class SerializationScopeReferenceHandler : ReferenceHandler
+    {
+        private readonly SerializationScopeReferenceResolver _resolver = new();
+
+        public override ReferenceResolver CreateResolver() => _resolver;
+    }
+
+    private class SerializationScopeReferenceResolver : ReferenceResolver
+    {
+        private uint _id;
+        private readonly Dictionary<string, object> _idToObjectMap = new();
+        private readonly Dictionary<object, string> _objectToIdMap = new();
+
+        public override void AddReference(string referenceId, object value)
+        {
+            if (!_idToObjectMap.TryAdd(referenceId, value))
+                throw new InvalidOperationException();
+        }
+
+        public override string GetReference(object value, out bool alreadyExists)
+        {
+            if (_objectToIdMap.TryGetValue(value, out var id))
+            {
+                alreadyExists = true;
+            }
+            else
+            {
+                _id++;
+                id = _id.ToString();
+                _objectToIdMap.Add(value, id);
+                alreadyExists = false;
+            }
+            return id;
+        }
+
+        public override object ResolveReference(string referenceId)
+        {
+            if (!_idToObjectMap.TryGetValue(referenceId, out var value))
+                throw new InvalidOperationException();
+            return value;
+        }
     }
 }
