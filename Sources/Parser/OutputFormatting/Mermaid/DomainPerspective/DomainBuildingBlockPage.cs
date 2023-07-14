e943120f3ad6f5596e6081d5ce114f13109efa9c
Marcin Markowski <marcin.markowski@itlibrium.com>
2023-07-14 10:28:27 +0200

    building blocks Mermaid pages moved to Modules directory

--- Sources/Parser/OutputFormatting/Mermaid/DomainPerspective/DomainBuildingBlockPage.cs
+++ Sources/Parser/OutputFormatting/Mermaid/DomainPerspective/DomainBuildingBlockPage.cs
@@ -33,8 +33,8 @@ public class DomainBuildingBlockPage : MermaidPageBase
 - related processes";
 
     public override string RelativeFilePath => _module is null
-        ? Path.Combine("BuildingBlocks", $"/{_buildingBlock.Name}.md")
-        : Path.Combine("BuildingBlocks", Path.Combine(_module.Id.Parts.ToArray()), $"{_buildingBlock.Name}.md");
+        ? Path.Combine("Modules", $"/{_buildingBlock.Name}.md")
+        : Path.Combine("Modules", Path.Combine(_module.Id.Parts.ToArray()), $"{_buildingBlock.Name}.md");
 
     public override Element MainElement => _buildingBlock;
 
