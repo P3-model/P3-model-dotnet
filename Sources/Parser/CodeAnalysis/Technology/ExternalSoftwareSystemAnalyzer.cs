19d9e96b06b8fb511e230093b61994dbbc9a5180
Marcin Markowski <marcin.markowski@itlibrium.com>
2023-07-21 16:50:54 +0200

    ExternalSoftwareSystemAnalyzer

---
+++ Sources/Parser/CodeAnalysis/Technology/ExternalSoftwareSystemAnalyzer.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using P3Model.Annotations.Technology;
+using P3Model.Parser.ModelSyntax;
+using P3Model.Parser.ModelSyntax.Technology;
+
+namespace P3Model.Parser.CodeAnalysis.Technology;
+
+[UsedImplicitly]
+public class ExternalSoftwareSystemAnalyzer : SymbolAnalyzer<INamedTypeSymbol>
+{
+    public void Analyze(INamedTypeSymbol symbol, ModelBuilder modelBuilder)
+    {
+        if (!symbol.TryGetAttribute(typeof(ExternalSoftwareSystemAttribute), out var externalSystemAttribute))
+            return;
+        var name = externalSystemAttribute.GetConstructorArgumentValue<string?>() ?? symbol.Name;
+        var externalSystem = new ExternalSoftwareSystem(name);
+        modelBuilder.Add(externalSystem, symbol);
+    }
+}
