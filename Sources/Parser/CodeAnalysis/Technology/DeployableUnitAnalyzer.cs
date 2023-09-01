8674edb00ce6cb3a42cae2dd6618924a3ab19062
Marcin Markowski <marcin.markowski@itlibrium.com>
2023-09-01 22:11:01 +0200

    relation between deployable unit and c# project

--- Sources/Parser/CodeAnalysis/Technology/DeployableUnitAnalyzer.cs
+++ Sources/Parser/CodeAnalysis/Technology/DeployableUnitAnalyzer.cs
@@ -6,6 +6,7 @@ using P3Model.Annotations.Technology;
 using P3Model.Parser.ModelSyntax;
 using P3Model.Parser.ModelSyntax.DomainPerspective.StaticModel;
 using P3Model.Parser.ModelSyntax.Technology;
+using P3Model.Parser.ModelSyntax.Technology.CSharp;
 
 namespace P3Model.Parser.CodeAnalysis.Technology;
 
@@ -19,23 +20,25 @@ public class DeployableUnitAnalyzer : SymbolAnalyzer<IAssemblySymbol>
         var name = deployableUnitAttribute.GetConstructorArgumentValue<string>(nameof(DeployableUnitAttribute.Name));
         var deployableUnit = new DeployableUnit(name);
         modelBuilder.Add(deployableUnit, symbol);
-        modelBuilder.Add(elements => GetRelations(symbol, deployableUnit, elements));
+        modelBuilder.Add(elements => GetRelationsToDomainModules(symbol, deployableUnit, elements));
+        modelBuilder.Add(elements => GetRelationsToCodeStructures(symbol, deployableUnit, elements));
     }
 
-    private static IEnumerable<Relation> GetRelations(IAssemblySymbol symbol, DeployableUnit deployableUnit,
-        ElementsProvider elements) => symbol
+    private static IEnumerable<Relation> GetRelationsToDomainModules(IAssemblySymbol symbol,
+        DeployableUnit deployableUnit, ElementsProvider elements) => symbol
         .GetReferencedAssembliesFromSameRepository()
         .SelectRecursively(assemblySymbol => assemblySymbol.GetReferencedAssembliesFromSameRepository())
-        .Distinct<IAssemblySymbol>(SymbolEqualityComparer.Default)
+        .Distinct<IAssemblySymbol>(CompilationIndependentSymbolEqualityComparer.Default)
         .SelectMany(assemblySymbol => elements
             .OfType<ElementInfo<DomainModule>>()
             .Where(info => info.Symbols.Contains(assemblySymbol) ||
                            info.Symbols.Any(s => s.IsFrom(assemblySymbol)) ||
-                           info.Directories.Any(d => d.ContainsSourcesOf(assemblySymbol))))
-        .Distinct()
-        .Select(elementInfo => new DomainModule.IsDeployedInDeployableUnit(elementInfo.Element, deployableUnit));
+                           info.Directories.Any(d => d.ContainsSourcesOf(assemblySymbol)))
+            .Select(elementInfo => new DomainModule.IsDeployedInDeployableUnit(elementInfo.Element, deployableUnit)));
 
-    private static bool AreRelated(ElementInfo elementInfo, ISymbol symbol) =>
-        elementInfo.Symbols.Contains(symbol) ||
-        symbol.SourcesAreInAny(elementInfo.Directories);
+    private static IEnumerable<Relation> GetRelationsToCodeStructures(ISymbol symbol,
+        DeployableUnit deployableUnit, ElementsProvider elements) => elements
+        .OfType<ElementInfo<CSharpProject>>()
+        .Where(info => info.Symbols.Contains(symbol))
+        .Select(elementInfo => new DeployableUnit.ContainsCSharpProject(deployableUnit, elementInfo.Element));
 }
--- Sources/Parser/ModelSyntax/Technology/DeployableUnit.cs
+++ Sources/Parser/ModelSyntax/Technology/DeployableUnit.cs
@@ -1,9 +1,11 @@
+using P3Model.Parser.ModelSyntax.Technology.CSharp;
+
 namespace P3Model.Parser.ModelSyntax.Technology;
 
 public record DeployableUnit(string Name) : Element
 {
     public Perspective Perspective => Perspective.Technology;
     
-    public record ContainsCodeStructure(DeployableUnit Source, CodeStructure Destination) 
-        : Relation<DeployableUnit, CodeStructure>;
+    public record ContainsCSharpProject(DeployableUnit Source, CSharpProject Destination) 
+        : Relation<DeployableUnit, CSharpProject>;
 }
