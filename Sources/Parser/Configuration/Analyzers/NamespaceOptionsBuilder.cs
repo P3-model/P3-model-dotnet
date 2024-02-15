af09e42f0de9ae9746a2d036845d2d4a3ed81db0
Marcin Markowski <marcin.markowski@itlibrium.com>
2024-02-15 18:08:07 +0100

    namespace filter fix

--- Sources/Parser/Configuration/Analyzers/NamespaceOptionsBuilder.cs
+++ Sources/Parser/Configuration/Analyzers/NamespaceOptionsBuilder.cs
@@ -12,11 +12,13 @@ namespace P3Model.Parser.Configuration.Analyzers;
 
 public class NamespaceOptionsBuilder
 {
-    private readonly List<Func<INamespaceSymbol, bool>> _namespacePredicates = new()
-    {
+    private const char NamespacePartSeparator = '.';
+
+    private readonly List<Func<INamespaceSymbol, bool>> _namespacePredicates =
+    [
         symbol => !AnnotatedWith<NotDomainModuleAttribute>(symbol)
-    };
-    private readonly List<Func<string, string>> _namespaceFilters = new();
+    ];
+    private readonly List<Func<string, string>> _namespaceFilters = [];
 
     [PublicAPI]
     public NamespaceOptionsBuilder OnlyFromAssembliesAnnotatedWith<TAttribute>() => Matching(
@@ -45,10 +47,10 @@ public class NamespaceOptionsBuilder
             if (symbol
                 .GetTypeMembers()
                 .Any(typeSymbol => typeSymbol.TryGetAttribute(typeof(TAttribute), out var attribute) &&
-                                   attribute.TryGetArgumentValue(nameof(NamespaceApplicable.ApplyOnNamespace),
-                                       out bool applyOnNamespace) &&
-                                   applyOnNamespace &&
-                                   (predicate is null || predicate(attribute))))
+                    attribute.TryGetArgumentValue(nameof(NamespaceApplicable.ApplyOnNamespace),
+                        out bool applyOnNamespace) &&
+                    applyOnNamespace &&
+                    (predicate is null || predicate(attribute))))
                 return true;
             symbol = symbol.ContainingNamespace;
         }
@@ -70,20 +72,35 @@ public class NamespaceOptionsBuilder
                 ? currentHierarchy[(rootNamespace.Length + 1)..]
                 : currentHierarchy);
 
+    // TODO: Extract namespaceFilters to separate types to test them without ISymbol
     [PublicAPI]
-    public NamespaceOptionsBuilder RemoveNamespacePart(params string[] layerNames) => Filter(initialHierarchy =>
-        layerNames.Aggregate(initialHierarchy,
-            (currentHierarchy, layerName) =>
+    public NamespaceOptionsBuilder RemoveNamespacePart(params string[] partsToRemove) => Filter(initialHierarchy =>
+        partsToRemove.Aggregate(initialHierarchy,
+            (currentHierarchy, partToRemove) =>
             {
-                if (currentHierarchy.Equals(layerName))
+                if (currentHierarchy.Equals(partToRemove))
                     return string.Empty;
-                var index = currentHierarchy.IndexOf(layerName, StringComparison.InvariantCulture);
-                return index switch
-                {
-                    -1 => currentHierarchy,
-                    0 => currentHierarchy[(layerName.Length + 1)..],
-                    _ => currentHierarchy.Remove(index - 1, layerName.Length + 1)
-                };
+                var partToRemoveStartIndex = currentHierarchy.IndexOf(partToRemove, StringComparison.InvariantCulture);
+                if (partToRemoveStartIndex == -1)
+                    return currentHierarchy;
+                var prevNamespacePartSeparatorIndex = currentHierarchy.LastIndexOf(NamespacePartSeparator,
+                    partToRemoveStartIndex);
+                var namespacePartStartIndex = prevNamespacePartSeparatorIndex == -1
+                    ? 0
+                    : prevNamespacePartSeparatorIndex + 1;
+                if (partToRemoveStartIndex != namespacePartStartIndex)
+                    return currentHierarchy;
+                var partToRemoveEndIndex = partToRemoveStartIndex + partToRemove.Length - 1;
+                var nextNamespacePartSeparatorIndex = currentHierarchy.IndexOf(NamespacePartSeparator,
+                    partToRemoveStartIndex);
+                var namespacePartEndIndex = nextNamespacePartSeparatorIndex == -1
+                    ? currentHierarchy.Length - 1
+                    : nextNamespacePartSeparatorIndex - 1;
+                if (partToRemoveEndIndex != namespacePartEndIndex)
+                    return currentHierarchy;
+                return partToRemoveStartIndex == 0 
+                    ? currentHierarchy[(partToRemove.Length + 1)..] 
+                    : currentHierarchy.Remove(partToRemoveStartIndex - 1, partToRemove.Length + 1);
             }));
 
     private NamespaceOptionsBuilder Filter(Func<string, string> filter)
