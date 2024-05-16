40d86ab45022a4ac92bbed49a57b7972fd35b28b
Marcin Markowski <marcin.markowski@itlibrium.com>
2024-05-16 18:05:51 +0200

    IComparable implementation for ElementId

--- Sources/Parser.ModelSyntax/ElementId.cs
+++ Sources/Parser.ModelSyntax/ElementId.cs
@@ -1,13 +1,19 @@
+using JetBrains.Annotations;
+
 namespace P3Model.Parser.ModelSyntax;
 
-public readonly record struct ElementId(string Value)
+public readonly record struct ElementId(string Value) : IComparable<ElementId>
 {
+    [PublicAPI]
     public static ElementId Create<TElement>(string idPartUniqueForElementType)
         where TElement : Element =>
         Create(typeof(TElement), idPartUniqueForElementType);
 
+    [PublicAPI]
     public static ElementId Create(Type type, string idPartUniqueForElementType) =>
         new($"{type.Name}|{idPartUniqueForElementType}".ToLowerInvariant());
+
+    public int CompareTo(ElementId other) => string.Compare(Value, other.Value, StringComparison.Ordinal);
 
     public override string ToString() => Value;
 }
